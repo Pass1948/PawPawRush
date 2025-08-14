8622d812c02e44251df2dd69d166834800d2d0fe
LeeYoungSin <dldudtls100@gmail.com>
2025-08-14 14:54:40 +0900

    초기커밋

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Resources;
+using UnityEngine;
+
+[DefaultExecutionOrder(-100)]       // GameManager가 다른 Script보다 먼저 호출되게 설정
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance => instance;
+
+    // Managers=========================
+
+    private static ResourceManager resourceManager;
+    public static ResourceManager Resource => resourceManager;
+
+    private static PoolManager poolManager;
+    public static PoolManager Pool => poolManager;
+
+    private static SceneManager sceneManager;
+    public static SceneManager Scene => sceneManager;
+
+    private static UIManager uiManager;
+    public static UIManager UI => uiManager;
+
+
+    private void Awake()
+    {
+        if (instance != null) { Destroy(this); return; }
+        instance = this;
+        DontDestroyOnLoad(this);
+        InitManagers();
+    }
+
+    private void OnDestroy()
+    {
+        if (instance == this)
+            instance = null;
+    }
+
+    private void InitManagers()
+    {
+        GameObject resourceObj = new GameObject("ResourceManager");
+        resourceObj.transform.SetParent(transform, false);
+        resourceManager = resourceObj.AddComponent<ResourceManager>();
+
+        GameObject poolObj = new GameObject("PoolManager");
+        poolObj.transform.SetParent(transform, false);
+        poolManager = poolObj.AddComponent<PoolManager>();
+
+        GameObject sceneObj = new GameObject("SceneManager");
+        sceneObj.transform.SetParent(transform, false);
+        sceneManager = sceneObj.AddComponent<SceneManager>();
+
+        GameObject uiObj = new GameObject("UIManager");
+        uiObj.transform.SetParent(transform, false);
+        uiManager = uiObj.AddComponent<UIManager>();
+    }
+
+}
---
+++ Assets/Scripts/UI/PopUpUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class PopUpUI : BaseUI
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+        }
+    }
