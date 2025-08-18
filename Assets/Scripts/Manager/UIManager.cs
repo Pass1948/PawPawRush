2ac0afba5673cdf41341f0f2aa822a5386a79c9e
슬기 <jsg8369@naver.com>
2025-08-18 16:41:47 +0900

    [Style]UIManager.CS 주석추가

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -4,13 +4,14 @@ using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// UI 관리 및 팝업/윈도우 UI의 생성, 표시, 닫기 등을 담당하는 매니저 클래스
 public class UIManager : MonoBehaviour
 {
-    private Canvas windowCanvas;
-    private Canvas popUpCanvas;
-    private Stack<PopUpUI> popUpStack;
+    private Canvas windowCanvas; // 윈도우 UI를 표시할 캔버스
+    private Canvas popUpCanvas; // 팝업 UI를 표시할 캔버스
+    private Stack<PopUpUI> popUpStack; // 팝업 UI의 스택 (여러 팝업을 관리)
 
-    private void Awake()
+    private void Awake() // 매니저 초기화, 캔버스 및 이벤트 시스템 생성
     {
         InstantsWindowUI();
         InstantsPopUpUI();
@@ -19,13 +20,13 @@ public class UIManager : MonoBehaviour
         popUpStack = new Stack<PopUpUI>();
     }
 
-    public void Recreated()
+    public void Recreated() // UI를 재생성할 때 호출
     {
         InstantsWindowUI();
         InstantsPopUpUI();
     }
 
-    public void InstantsWindowUI()
+    public void InstantsWindowUI() // 윈도우 UI용 캔버스 생성 및 설정
     {
         if (windowCanvas == null)
         {
@@ -35,7 +36,7 @@ public class UIManager : MonoBehaviour
             DontDestroyOnLoad(windowCanvas.gameObject);
         }
     }
-    public void InstantsPopUpUI()
+    public void InstantsPopUpUI() // 팝업 UI용 캔버스 생성 및 설정
     {
         if (popUpCanvas == null)
         {
@@ -48,7 +49,7 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    public void EnsureEventSystem()
+    public void EnsureEventSystem() // 이벤트 시스템이 없을 경우 생성
     {
         if (EventSystem.current != null)
             return;
@@ -61,31 +62,31 @@ public class UIManager : MonoBehaviour
 
     // --------------[WindowUI]--------------
 
-    public T ShowWindowUI<T>(T windowUI) where T : WindowUI
+    public T ShowWindowUI<T>(T windowUI) where T : WindowUI // 윈도우 UI를 표시 (오브젝트 직접 전달)
     {
         T ui = GameManager.Pool.GetUI(windowUI);
         ui.transform.SetParent(windowCanvas.transform, false);
         return ui;
     }
 
-    public T ShowWindowUI<T>(string path) where T : WindowUI
+    public T ShowWindowUI<T>(string path) where T : WindowUI // 윈도우 UI를 표시 (리소스 경로로 로드)
     {
         T ui = GameManager.Resource.Load<T>(path);
         return ShowWindowUI(ui);
     }
 
-    public void CloseWindowUI(WindowUI windowUI)
+    public void CloseWindowUI(WindowUI windowUI) // 윈도우 UI 닫기
     {
         GameManager.Pool.ReleaseUI(windowUI.gameObject);
     }
 
-    public void SelectWindowUI<T>(T windowUI) where T : WindowUI
+    public void SelectWindowUI<T>(T windowUI) where T : WindowUI // 윈도우 UI를 최상단으로 선택
     {
         windowUI.transform.SetAsLastSibling();
     }
 
     // --------------[PopUpUI]--------------
-    public T ShowPopUpUI<T>(T popUpUI) where T : PopUpUI
+    public T ShowPopUpUI<T>(T popUpUI) where T : PopUpUI // 팝업 UI를 표시 (리소스 경로로 로드)
     {
         if (popUpStack.Count > 0)
         {
@@ -101,13 +102,13 @@ public class UIManager : MonoBehaviour
         return ui;
     }
 
-    public T ShowPopUpUI<T>(string path) where T : PopUpUI
+    public T ShowPopUpUI<T>(string path) where T : PopUpUI // 팝업 UI를 표시 (리소스 경로로 로드)
     {
         T ui = GameManager.Resource.Load<T>(path);
         return ShowPopUpUI(ui);
     }
 
-    public void ClosePopUpUI()
+    public void ClosePopUpUI() // 현재 팝업 UI 닫기
     {
         PopUpUI ui = popUpStack.Pop();
         GameManager.Pool.Release(ui.gameObject);
@@ -118,7 +119,8 @@ public class UIManager : MonoBehaviour
             curUI.gameObject.SetActive(true);
         }
     }
-    public void PopUpUIClear()
+
+    public void PopUpUIClear() // 모든 팝업 UI 스택 초기화
     {
         popUpStack.Clear();
     }
