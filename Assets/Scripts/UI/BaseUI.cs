a0c004e45c20a461c431248bb0771aa7ee639b3e
슬기 <jsg8369@naver.com>
2025-08-18 17:15:33 +0900

    [Style]BaseUI.CS 주석추가

--- Assets/Scripts/UI/BaseUI.cs
+++ Assets/Scripts/UI/BaseUI.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+/// UI 오브젝트의 자식 컴포넌트(Button, Text 등)를 자동으로 바인딩하는 기본 UI 클래스
 
     public class BaseUI : MonoBehaviour
     {
-        protected Dictionary<string, RectTransform> rectTransform;
-        protected Dictionary<string, Button> buttons;
-        protected Dictionary<string, TMP_Text> texts;
-        protected virtual void Awake()
+        protected Dictionary<string, RectTransform> rectTransform; // 자식 RectTransform을 이름으로 관리하는 딕셔너리
+        protected Dictionary<string, Button> buttons; // 자식 Button을 이름으로 관리하는 딕셔너리
+        protected Dictionary<string, TMP_Text> texts; // 자식 TMP_Text를 이름으로 관리하는 딕셔너리
+        protected virtual void Awake() // 오브젝트가 생성될 때 자식 컴포넌트 바인딩
         {
             BindChildren();
         }
         
-        protected virtual void OnDisable() {}
+        protected virtual void OnDisable() {} // 오브젝트가 비활성화될 때 호출 (추후 확장용)
 
         private void BindChildren()
+            // 자식 RectTransform, Button, TMP_Text를 이름 기준으로 딕셔너리에 바인딩
+
         {
             rectTransform = new Dictionary<string, RectTransform>();
             buttons = new Dictionary<string, Button>();
