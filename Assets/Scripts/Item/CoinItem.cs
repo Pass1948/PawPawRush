ea3a13a2660cab15f543d888aa6d53fb22b9d970
LeeYoungSin <dldudtls100@gmail.com>
2025-08-21 11:38:40 +0900

    [Feat] 코인 사운드 추가

--- Assets/Scripts/Item/CoinItem.cs
+++ Assets/Scripts/Item/CoinItem.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class CoinItem : MonoBehaviour
 {
     [SerializeField] private int scoreAmount = 1;
-
+    [SerializeField] AudioClip soundClip;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             GameManager.Score.AddScore(scoreAmount);
-
+            AudioSource.PlayClipAtPoint(soundClip, transform.position);
             // TODO: 오브젝트 풀링
             Destroy(gameObject); // 아이템은 사용 후 제거
         }
